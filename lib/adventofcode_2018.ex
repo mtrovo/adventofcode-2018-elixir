5de908398b61383e7457f5b18f1e39359d8ece81
Moises Trovo <mtrovo@tuta.io>
2018-12-02 13:34:12 +0100

    Add Day 01 Chronal Calibration Part 02

--- lib/adventofcode_2018.ex
+++ lib/adventofcode_2018.ex
@@ -12,7 +12,6 @@ defmodule Adventofcode2018 do
   @doc false
   def read_puzzle_input_for(module, options) do
     fname = Path.join(["input", input_filename(module) <> ".txt"])
-    IO.puts("Reading file " <> fname)
     case File.read(fname) do
       {:ok, data} -> trim(data, Keyword.get(options, :trim))
       {:error, _} -> nil
