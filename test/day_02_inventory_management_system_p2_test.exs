ad5042e50cf15b3490f4318f6b40e91f84e9132a
Moises Trovo <mtrovo@tuta.io>
2018-12-02 15:45:13 +0100

    Add Day 02 Inventory Management System part 2

---
+++ test/day_02_inventory_management_system_p2_test.exs
@@ -0,0 +1,15 @@
+defmodule Adventofcode2018.Day02InventoryManagementSystemP2Test do
+  use Adventofcode2018.AdventCase
+  import Adventofcode2018.Day02InventoryManagementSystemP2
+  describe "common_letters_correct_ids/1" do
+    test "find common suffix" do
+      assert "bc" = "aaa abc bbc" |> common_letters_correct_ids()
+    end
+    test "find common prefix" do
+      assert "aa" = "aaa aab bbc" |> common_letters_correct_ids()
+    end
+    test_with_puzzle_input do
+      assert "krdmtuqjgwfoevnaboxglzjph" = puzzle_input() |> common_letters_correct_ids()
+    end
+  end
+end
