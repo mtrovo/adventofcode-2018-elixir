ebcb28bacb0ed4865c5ed23a1df17e463058f280
Moises Trovo <mtrovo@tuta.io>
2018-12-06 11:44:58 +0100

    Add Day 05 Alchemical Reduction, part 2

---
+++ lib/day_05_alchemical_reduction_p2.ex
@@ -0,0 +1,23 @@
+defmodule Adventofcode2018.Day05AlchemicalReductionP2 do
+  use Adventofcode2018
+  import Adventofcode2018.Day05AlchemicalReductionP1
+
+  def remove_letter([], _) do [] end
+  def remove_letter([c | cs], rm) when c - rm |> abs == 32 do remove_letter(cs, rm) end
+  def remove_letter([c | cs], c) do remove_letter(cs, c) end
+  def remove_letter([c | cs], rm) do
+    [c | remove_letter(cs, rm)]
+  end
+  
+  def rm_unit_length(input) do
+    polymer = input
+    |> String.trim
+    |> String.to_charlist
+
+    range = ?a..?z
+    |> Enum.to_list
+    |> Stream.map(&remove_letter(polymer, &1))
+    |> Stream.map(&react_length/1)
+    |> Enum.min
+  end
+end
---
+++ test/day_05_alchemical_reduction_p2_test.exs
@@ -0,0 +1,25 @@
+defmodule Adventofcode2018.Day05AlchemicalReductionP2Test do
+  use Adventofcode2018.AdventCase
+  import Adventofcode2018.Day05AlchemicalReductionP2
+
+  describe "remove_letter/2" do
+    test "base case: empty" do
+      assert [] = remove_letter([], ?a)
+    end
+    test "remove same case" do
+      assert ~c(b) = remove_letter(~c(ab), ?a)
+    end
+    test "remove diff case" do
+      assert ~c(b) = remove_letter(~c(Ab), ?a)
+    end
+
+    test "remove all letters" do
+      assert ~c(b) = remove_letter(~c(aaaAAAbaaaAAA), ?a)
+    end
+  end
+  describe "rm_unit_length/1" do
+    test_with_puzzle_input do
+      assert 1337 = puzzle_input() |> rm_unit_length()
+    end
+  end
+end
