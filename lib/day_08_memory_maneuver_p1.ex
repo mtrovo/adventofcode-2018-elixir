a2a46b2a6b10627a40ad043750456214a309a59b
Moises Trovo <mtrovo@tuta.io>
2018-12-09 00:39:26 +0100

    Add Day 8: Memory Maneuver, part 1

---
+++ lib/day_08_memory_maneuver_p1.ex
@@ -0,0 +1,48 @@
+defmodule Adventofcode2018.Day08MemoryManeuverP1 do
+  use Adventofcode2018
+
+  defmodule Node do
+    defstruct [:nr_child, :metadata]
+  end
+
+  def node(nr_child, metadata) do
+    %Node{nr_child: nr_child, metadata: metadata}
+  end
+
+  def parse_input(input) do
+    input
+    |> String.split(" ", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def parse_nodes(input, nodes \\ [])
+
+  def parse_nodes([], nodes) do
+    {[], nodes}
+  end
+
+  def parse_nodes([childs, metadata_size | tail], nodes) do
+    {tail, nodes} =
+    if childs > 0 do
+      1..childs
+      |> Enum.reduce({tail, nodes}, fn (_, {t, n}) -> parse_nodes(t, n) end)
+    else
+      {tail, nodes}
+    end
+    {metadata, tail} = Enum.split(tail, metadata_size)
+    {tail, [node(childs, metadata) | nodes]}
+  end
+
+  def nodes(input) do
+    {_, nodes} = parse_nodes(input)
+    nodes
+  end
+
+  def metadata_sum(input) do
+    input
+    |> parse_input
+    |> nodes
+    |> Enum.flat_map(&(&1.metadata))
+    |> Enum.sum()
+  end
+end
---
+++ test/day_08_memory_maneuver_p1_test.exs
@@ -0,0 +1,32 @@
+defmodule Adventofcode2018.Day08MemoryManeuverP1Test do
+  use Adventofcode2018.AdventCase
+  import Adventofcode2018.Day08MemoryManeuverP1
+
+  describe "parse_input/1" do
+    test "return a list of integers" do
+      assert [2, 3, 0, 3] = parse_input("2 3 0 3")
+    end
+  end
+
+  describe "parse_nodes/2" do
+    nodes = [
+      node(2, [1,1,2]),
+      node(1, [2]),
+      node(0, [99]),
+      node(0, [10, 11, 12])
+    ]
+    assert {[], ^nodes} = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2"
+    |> parse_input
+    |> parse_nodes
+  end
+
+  describe "metadata_sum/1" do
+    test "sample input" do
+      assert 138 = "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2" |> metadata_sum()
+    end
+
+    test_with_puzzle_input do
+      assert 1337 = puzzle_input() |> metadata_sum()
+    end
+  end
+end
